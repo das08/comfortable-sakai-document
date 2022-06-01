6aa9d4a1f5695a61b0fa27af73a777fb0ce0b9e2
das08 <41512077+das08@users.noreply.github.com>
2022-06-01 13:38:35 +0900

    fix

---
+++ docs/project-structure/CodeSandboxExample.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+export default function CodeSandboxExample({ name }: { name: string }): JSX.Element {
+    const { siteConfig } = useDocusaurusContext();
+
+    // const path = `${siteConfig.organizationName}/${siteConfig.projectName}/tree/${siteConfig.customFields.branch}/examples/${name}`;
+    const path = "kyoto-u/comfortable-sakai/tree/master/examples/miniSakai";
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href={`https://codesandbox.io/s/github/${path}`}>Open in CodeSandbox</a>
+                </li>
+                <li>
+                    <a href={`https://github.com/${path}`}>Show Source</a>
+                </li>
+            </ul>
+            <iframe
+                title="Example Preview"
+                className="embedded-iframe"
+                src={`https://codesandbox.io/embed/github/${path}?autoresize=1&fontsize=14&theme=light`}
+            >
+                Example Preview not available
+            </iframe>
+        </>
+    );
+}
