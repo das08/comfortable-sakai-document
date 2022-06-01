57d9792266249c108ac1bc2dfbc8dcebe192be6d
das08 <41512077+das08@users.noreply.github.com>
2022-06-01 13:46:09 +0900

    add

--- examples/miniSakai/src/minisakai.tsx
+++ examples/miniSakai/src/minisakai.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { EntryUnion, MiniSakaiEntryList } from "../../comfortable-sakai-components/src/components/entryTab";
-import { Settings } from "../../comfortable-sakai-components/src/features/setting/types";
-import { Course } from "../../comfortable-sakai-components/src/features/course/types";
-import { AssignmentEntry } from "../../comfortable-sakai-components/src/features/entity/assignment/types";
+import { EntryUnion, MiniSakaiEntryList } from "../../comfortable-sakai-component/src/components/entryTab";
+import { Settings } from "../../comfortable-sakai-component/src/features/setting/types";
+import { Course } from "../../comfortable-sakai-component/src/features/course/types";
+import { AssignmentEntry } from "../../comfortable-sakai-component/src/features/entity/assignment/types";
 
 import "./comfortable-sakai.css";
 
